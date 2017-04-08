b6febe4ee23dfbc14f1e42f732924d7afe858b06
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-04-08 13:26:40 -0700

    add additional statement if time is greater than 150

--- TurkeyTimer/main.c
+++ TurkeyTimer/main.c
@@ -11,6 +11,10 @@
 void showCookTimeForTurkey(int pounds) {
   int necessaryMinutes = 15 + 15 * pounds;
   printf("Cook for %d minutes.\n", necessaryMinutes);
+  if (necessaryMinutes > 120) {
+    int halfway = necessaryMinutes / 2;
+    printf("Rotate after %d of the %d minutes.\n", halfway, necessaryMinutes);
+  }
 }
 
 int main(int argc, const char * argv[]) {
